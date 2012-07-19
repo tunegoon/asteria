5cf21777d5693b6f2566cc8e235ab86d51306e87
Loran Briggs <loranbriggs@gmail.com>
2012-07-19 15:46:20 -0700

    small changes

--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
-# Create your views here.
+from django.core.context_processors import csrf
 from django.shortcuts import render
+from app.models import Name
+
 
 def home(request):
-    c = {'content':'The start of Project Asteria'}
+    names = Name.objects.all()
+    c = {'names': names}
+    if request.method == 'POST':
+        first = request.POST.get('first')
+        if first:
+            name = Name()
+            name.first = first
+            name.save()
+    c.update(csrf(request))
     return render(request, 'home.html', c)
